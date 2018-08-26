5106dd0bab87f0b4e7b2e3ee2f9d551310a3320e
lyn <liuyanan113@163.com>
2018-08-26 23:56:41 +0800

    complete food rating

---
+++ src/components/ratinglist/ratinglist.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="rating">
+  <ul v-if="props.ratings && props.ratings.length">
+    <li v-for="(item,index) in props.ratings" :key="index" class="rating-item"></li>
+  </ul>
+  <div v-else>暂无评论！</div>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    ratings: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+
+</style>
