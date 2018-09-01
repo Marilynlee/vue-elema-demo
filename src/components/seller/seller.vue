a76b112476d3b9957ee9e73d970d31d13d6127b2
lyn <liuyanan113@163.com>
2018-09-02 00:17:57 +0800

    compelte seller part

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,6 +28,10 @@
             </div>
           </li>
         </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon icon-favorite" :class="{'active':favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
       </div>
       <split></split>
       <div class="bulletin">
@@ -39,6 +43,24 @@
           <li class="support-item  border-1px" v-for="(item,index) in seller.supports" :key="index">
             <imgText :pros="{show:true,size:'16',type:item.type,bg:'nobg',text:item.description}"></imgText>
           </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="seller-pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pics-wrapper" ref="picWrapper">
+          <ul class="pics-list" ref="picList">
+            <li class="pic-item" v-for="(pic, index) in seller.pics" :key="index">
+              <img :src="pic" alt="img" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="(info,index) in seller.infos" :key="index">{{info}}</li>
         </ul>
       </div>
     </div>
@@ -67,13 +89,87 @@
       imgText,
       split
     },
+    data() {
+      return {
+        favorite: false
+      };
+    },
+    computed: {
+      favoriteText() {
+        return this.favorite ? '已收藏' : '收藏';
+      }
+    },
+    /* beforeCreate() {
+      console.log(this, 0);
+    }, */
     created() {
+      console.log(this, 1);
       this.$nextTick(() => {
+        console.log(this.seller, 2);
         this.scroll = new BScroll(this.$refs.seller, {
           click: true
         });
+        this._initPicScroll();
       });
+    },
+    methods: {
+      _initPicScroll() {
+        if (this.seller.pics) {
+          console.log('methods');
+          let picWidth = 120;
+          let margin = 6;
+          let width = (picWidth + margin) * this.seller.pics.length - margin;
+          this.$refs.picList.style.width = width + 'px';
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$refs.picWrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              });
+            } else {
+              this.picScroll.refresh();
+            }
+          });
+        }
+      },
+      toggleFavorite() {
+        this.favorite = !this.favorite;
+      }
+    },
+    /* beforeMount() {
+      console.log(this.seller, 3);
+      this.$nextTick(() => {
+        console.log(this.seller, 4);
+      });
+    },
+    mounted() {
+      console.log(this.seller, 5);
+      this.$nextTick(() => {
+        console.log(this.seller, 6);
+      });
+    }, */
+    beforeUpdate() {
+      console.log(this.seller, 7);
+      this._initPicScroll();
     }
+    /* updated() {
+      console.log(this.seller, 9);
+      this.$nextTick(() => {
+        console.log(this.seller, 10);
+      });
+    },
+    beforeDestroy() {
+      console.log(this.seller, 11);
+      this.$nextTick(() => {
+        console.log(this.seller, 12);
+      });
+    },
+    destroyed() {
+      console.log(this.seller, 13);
+      this.$nextTick(() => {
+        console.log(this.seller, 14);
+      });
+    } */
   };
 </script>
 
@@ -87,6 +183,7 @@
   width: 100%
   overflow :hidden
   .overview
+    position: relative
     padding: 18px 18px 0 18px
     .title
       margin-bottom: 8px
@@ -128,6 +225,24 @@
           color:rgb(7,17,27)
           .stress
             font-size:24px
+    .favorite
+      position: absolute
+      width: 48px
+      top: 18px
+      right: 13px
+      text-align: center
+      .icon
+        display: block
+        margin-bottom: 4px
+        line-height: 24px
+        font-size: 24px
+        color:#d4d6d9
+        &.active
+          color:rgb(240,20,20)
+      .text
+        line-height: 10px
+        font-size: 10px
+        color:rgb(77,85,93)
   .bulletin
     padding:18px 18px 0 18px
     .title
@@ -147,4 +262,39 @@
         padding: 16px 12px
         font-size: 0
         border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border:none
+  .seller-pics
+    padding: 18px
+    .title
+      margin-bottom: 12px
+      line-height: 14px
+      font-size: 14px
+      color:rgb(7,17,27)
+    .pics-wrapper
+      width: 100%
+      overflow: hidden
+      white-space :nowrap
+      .pics-list
+        font-size: 0
+        .pic-item
+          display: inline-block
+          margin-right: 6px
+          width: 120px
+          height: 90px
+  .info
+    padding: 18px 18px 0 18px
+    color:rgb(7,17,27)
+    .title
+      padding-bottom: 12px
+      line-height: 14px
+      border-1px(rgba(7,17,27,0.1))
+      font-size: 14px
+    .info-item
+      padding: 16px 12px
+      line-height: 16px
+      border-1px(rgba(7,17,27,0.1))
+      font-size: 12px
+      &:last-child
+        border:none
 </style>
